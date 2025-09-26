d828b55873cb94a57a68999b6a942dd5b60a1720
CooperStannard <03cooper.stannard@kamsc.org>
2025-09-26 19:15:43 -0400

    int

--- src/main/java/DistanceMultiple.java
+++ src/main/java/DistanceMultiple.java
@@ -30,31 +30,30 @@
 import java.util.Scanner;
 
 
-public class DistanceMultiple {
-    public static void main(String[] args) {
+public class DistanceMultiple
+{
+    public static void main(String[] args)
+    {
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter("[()xy,\\s]+");
-
+        double xCord1, xCord2, yCord1, yCord2, distance;
         // Signature
         System.out.println("Cooper Stannard\nAPCS-A\n2.8\n");
 
         System.out.print("Enter number of runs:  ");
-        double numruns = sc.nextDouble();
+        int numruns = sc.nextInt();
 
         for (int k = 0; k < numruns; k++)
         {
             System.out.print("\nEnter two coordinates:  ");
-        }
-            for (int i = 0; i< numruns; i++)
-            {
-                double xCord1 = sc.nextDouble();
-                double yCord1 = sc.nextDouble();
-                double xCord2 = sc.nextDouble();
-                double yCord2 = sc.nextDouble();
+            xCord1 = sc.nextDouble();
+            yCord1 = sc.nextDouble();
+            xCord2 = sc.nextDouble();
+            yCord2 = sc.nextDouble();
 
-                // Calculates the distance formula and prints the number
-                double distance = Math.pow(Math.pow(xCord1 - xCord2, 2) + Math.pow(yCord1 - yCord2, 2), 1.0 / 2);
-                System.out.println("The distance is " + distance);
-            }
+            // Calculates the distance formula and prints the number
+            distance = Math.pow(Math.pow(xCord1 - xCord2, 2) + Math.pow(yCord1 - yCord2, 2), 1.0 / 2);
+            System.out.println("The distance is " + distance);
         }
     }
+}
