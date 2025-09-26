090aac28b620896260a085079555ab448f31d038
CooperStannard <03cooper.stannard@kamsc.org>
2025-09-26 19:02:28 -0400

    thing

--- src/main/java/DistanceMultiple.java
+++ src/main/java/DistanceMultiple.java
@@ -36,23 +36,25 @@ public class DistanceMultiple {
         sc.useDelimiter("[()xy,\\s]+");
 
         // Signature
-        System.out.println("Cooper Stannard\nAPCS-A\n2.8\n\n");
+        System.out.println("Cooper Stannard\nAPCS-A\n2.8\n");
 
-        //Asks for number of runs
         System.out.print("Enter number of runs:  ");
         double numruns = sc.nextDouble();
-        for (int k = 0; k < numruns; k++) {
-            //Reads in all numbers
+
+        for (int k = 0; k < numruns; k++)
+        {
             System.out.print("\nEnter two coordinates:  ");
-            double xCord1 = sc.nextDouble();
-            double yCord1 = sc.nextDouble();
-            double xCord2 = sc.nextDouble();
-            double yCord2 = sc.nextDouble();
+        }
+            for (int i = 0; i< numruns; i++)
+            {
+                double xCord1 = sc.nextDouble();
+                double yCord1 = sc.nextDouble();
+                double xCord2 = sc.nextDouble();
+                double yCord2 = sc.nextDouble();
 
-            // Calculates the distance formula and prints the number
-            double distance = Math.pow(Math.pow(xCord1 - xCord2, 2) + Math.pow(yCord1 - yCord2, 2), 1.0 / 2);
-            System.out.println("The distance is " + distance);
-
+                // Calculates the distance formula and prints the number
+                double distance = Math.pow(Math.pow(xCord1 - xCord2, 2) + Math.pow(yCord1 - yCord2, 2), 1.0 / 2);
+                System.out.println("The distance is " + distance);
+            }
         }
     }
-}
